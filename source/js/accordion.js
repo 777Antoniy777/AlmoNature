958f56dc3e1492f367d5341be62279762ad0a25a
anton_kuzmitsky <tat7106@gmail.com>
2019-10-29 13:33:20 +0600

    done project

--- source/js/accordion.js
+++ source/js/accordion.js
@@ -1,23 +1,13 @@
 'use strict';
 (function () {
-  // var sectionList = document.querySelector('.section__nav');
-  // var adressList = document.querySelector('.adress__list');
-  // var sectionButtonWrapper = document.querySelector('.section__wrapper');
-  // var adressButtonWrapper = document.querySelector('.adress__wrapper');
-  // var sectionButton = document.querySelector('.section__wrapper button');
-  // var adressButton = document.querySelector('.adress__wrapper button');
-
-  const footerList = document.querySelector('.footer__link-list');
-  const footerSublist = footerList.querySelectorAll('.footer__link-sublist');
-  const footerSublistArray = Array.from(footerSublist);
-
-  console.log(footerList, footerSublist, footerSublistArray);
-
+  var footerList = document.querySelector('.footer__link-list');
+  var footerSublist = footerList.querySelectorAll('.footer__link-sublist');
+  var footerSublistArray = Array.from(footerSublist);
 
   var mediaQueryList = window.matchMedia("(max-width: 767px)");
   function handleMediaChange(evt) {
     if (evt.matches) {
-      footerSublistArray.map((elem) => {
+      footerSublistArray.map(function(elem) {
         elem.classList.remove('no-js');
       });
     }
@@ -31,9 +21,10 @@
 
   function openAccordionListHandler(evt) {
     evt.preventDefault();
-    const target = evt.target;
-    const sublistTitle = target.closest('h3');
-    const sublist = sublistTitle.nextElementSibling;
+
+    var target = evt.target;
+    var sublistTitle = target.closest('.footer__wrapper-title');
+    var sublist = sublistTitle.nextElementSibling;
 
     if (window.matchMedia("(max-width: 767px)").matches) {
 
@@ -50,11 +41,11 @@
 
     if (list.style.maxHeight) {
       list.style.maxHeight = null;
+      title.classList.remove('accordion-open');
     } else {
       list.style.maxHeight = list.scrollHeight + 'px';
+      title.classList.add('accordion-open');
     }
 
-    title.classList.toggle('accordion-close');
-    title.classList.toggle('accordion-open');
   }
 })();
