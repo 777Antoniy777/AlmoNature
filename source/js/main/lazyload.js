a692990454f553bfb3736911d2436bd48edd80ae
anton_kuzmitsky <tat7106@gmail.com>
2019-12-02 17:42:03 +0600

    tested lazyload script, it works

--- source/js/main/lazyload.js
+++ source/js/main/lazyload.js
@@ -1,10 +1,3 @@
-// var bLazy = new Blazy({
-//   selector: 'img',
-// });
-
-// window.bLazy = new Blazy({
-//   selector: 'test',
-//   success: function(element){
-//     console.log("Element loaded: ", element.nodeName);
-//   }
-// });
+var bLazy = new Blazy({
+  selector: '.b-lazy',
+});
